119014a9f520ad391c1cfbf04636e80436bd1b3d
lutzzdias <thiagolutzdias@hotmail.com>
2023-02-07 16:43:42 -0400

    Setup inicial do projeto

---
+++ android/app/src/main/kotlin/com/sesims/cis/eldorado/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.sesims.cis.eldorado
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Eldorado',
+      theme: ThemeData(
+        primarySwatch: Colors.green,
+      ),
+      home: const HomeScreen(),
+    );
+  }
+}
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Eldorado'),
+      ),
+      body: const Center(
+        child: Text('Eldorado'),
+      ),
+    );
+  }
+}
