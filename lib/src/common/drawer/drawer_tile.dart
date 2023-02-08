ccdad4ca92f7a5f23fac2719af1c018eff1169db
lutzzdias <thiagolutzdias@hotmail.com>
2023-02-08 11:14:16 -0400

    Criado widget de botão do menu drawer

---
+++ lib/src/common/drawer/drawer_tile.dart
@@ -0,0 +1,48 @@
+import 'package:eldorado/src/constants/app_sizes.dart';
+import 'package:eldorado/src/constants/custom_colors.dart';
+import 'package:flutter/material.dart';
+
+class DrawerTile extends StatelessWidget {
+  final String title;
+  final IconData icon;
+
+  const DrawerTile({
+    super.key,
+    required this.title,
+    required this.icon,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(bottom: Sizes.p8),
+      child: OutlinedButton(
+        onPressed: () {},
+        style: OutlinedButton.styleFrom(
+          side: const BorderSide(
+            width: 1.5,
+            color: CustomColors.darkGreen,
+          ),
+          foregroundColor: CustomColors.darkGreen,
+          textStyle: const TextStyle(fontSize: 20),
+          padding: const EdgeInsets.symmetric(
+            vertical: Sizes.p4,
+            horizontal: Sizes.p8,
+          ),
+        ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            Icon(
+              icon,
+              color: CustomColors.darkGreen,
+              size: 35,
+            ),
+            gapW4,
+            Text(title),
+          ],
+        ),
+      ),
+    );
+  }
+}
