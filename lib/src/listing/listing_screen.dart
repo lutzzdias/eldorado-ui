b3d18aa29445fd3b689b70d7af3a687e70eb2504
Thiago Lütz Dias <estagio.sst19@sesims.com.br>
2023-02-09 09:29:05 -0400

    Create APRs list screen

--- lib/src/listing/listing_screen.dart
+++ lib/src/listing/listing_screen.dart
@@ -1,7 +1,9 @@
 import 'package:eldorado/src/base/base_screen.dart';
 import 'package:eldorado/src/common/drawer/custom_drawer.dart';
+import 'package:eldorado/src/common/screen_title.dart';
 import 'package:eldorado/src/constants/app_sizes.dart';
 import 'package:eldorado/src/constants/custom_colors.dart';
+import 'package:eldorado/src/listing/companies_listview.dart';
 import 'package:flutter/material.dart';
 
 class ListingScreen extends StatelessWidget {
@@ -25,38 +27,76 @@ class ListingScreen extends StatelessWidget {
             left: 45,
             right: 45,
             child: Column(
+              mainAxisSize: MainAxisSize.min,
               crossAxisAlignment: CrossAxisAlignment.stretch,
               children: [
-                Container(
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(5),
-                    color: CustomColors.yellow,
-                  ),
-                  height: 50,
-                  alignment: Alignment.centerLeft,
-                  padding: const EdgeInsets.only(left: Sizes.p16),
-                  child: const Text(
-                    'Lista de APR\'s',
-                    style: TextStyle(
-                      color: CustomColors.darkGreen,
-                      fontSize: 20,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                ),
+                const ScreenTitle(title: 'Lista de APR\'s'),
+                gapH12,
                 Row(
                   children: [
-                    const Expanded(child: TextField()),
+                    Expanded(
+                      child: SizedBox(
+                        height: 35,
+                        child: TextField(
+                          textAlign: TextAlign.start,
+                          textAlignVertical: TextAlignVertical.bottom,
+                          decoration: InputDecoration(
+                            hintText: 'Buscar APR',
+                            hintStyle: const TextStyle(color: Colors.black26),
+                            suffixIcon: const Icon(Icons.search),
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(30),
+                              borderSide: const BorderSide(
+                                style: BorderStyle.solid,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                    gapW12,
                     OutlinedButton(
                       onPressed: () {},
+                      style: OutlinedButton.styleFrom(
+                        foregroundColor: Colors.black38,
+                        side: const BorderSide(
+                          color: Colors.black38,
+                          width: 1,
+                        ),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(30),
+                        ),
+                      ),
                       child: Row(
                         children: const [
                           Text('Filtros'),
+                          gapW4,
                           Icon(Icons.list),
                         ],
                       ),
                     ),
                   ],
+                ),
+                const CompaniesListView(),
+                ElevatedButton.icon(
+                  onPressed: () {},
+                  icon: const Icon(
+                    Icons.add,
+                  ),
+                  label: const Text('Adicionar APR'),
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: CustomColors.darkGreen,
+                    foregroundColor: Colors.white,
+                    padding: const EdgeInsets.all(Sizes.p12),
+                    elevation: 10,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    textStyle: const TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.w500,
+                    ),
+                  ),
                 ),
               ],
             ),
