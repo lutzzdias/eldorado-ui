7bf1497e1eb9016cee7e9bd1fec16c4bb2821d76
Thiago Lütz Dias <estagio.sst19@sesims.com.br>
2023-02-09 09:28:51 -0400

    Create reusable screen title widget

---
+++ lib/src/common/screen_title.dart
@@ -0,0 +1,43 @@
+import 'package:eldorado/src/constants/app_sizes.dart';
+import 'package:eldorado/src/constants/custom_colors.dart';
+import 'package:flutter/material.dart';
+
+class ScreenTitle extends StatelessWidget {
+  final String title;
+  final Color backgroundColor;
+  final Color textColor;
+  final double height;
+  final double fontSize;
+  final double borderRadius;
+
+  const ScreenTitle({
+    super.key,
+    required this.title,
+    this.backgroundColor = CustomColors.yellow,
+    this.textColor = CustomColors.darkGreen,
+    this.height = 50,
+    this.fontSize = 20,
+    this.borderRadius = 5,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(borderRadius),
+        color: backgroundColor,
+      ),
+      height: height,
+      alignment: Alignment.centerLeft,
+      padding: const EdgeInsets.only(left: Sizes.p16),
+      child: Text(
+        title,
+        style: TextStyle(
+          color: textColor,
+          fontSize: fontSize,
+          fontWeight: FontWeight.bold,
+        ),
+      ),
+    );
+  }
+}
