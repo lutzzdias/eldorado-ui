fb2938315dd34b221000ffd8d889d5d072e9e64c
lutzzdias <thiagolutzdias@hotmail.com>
2023-02-09 15:06:09 -0400

    Create custom nav bar widget

---
+++ lib/src/common/footer/custom_nav_bar.dart
@@ -0,0 +1,49 @@
+import 'package:eldorado/src/constants/custom_colors.dart';
+import 'package:flutter/material.dart';
+
+class CustomNavBar extends StatelessWidget {
+  final List<BottomNavigationBarItem> items;
+  final List<String> screenNames;
+
+  const CustomNavBar({
+    super.key,
+    required this.items,
+    required this.screenNames,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return BottomNavigationBar(
+      currentIndex: 0,
+      onTap: (index) {
+        switch (index) {
+          case 0:
+            Navigator.of(context).pushNamed(screenNames[index]);
+            break;
+          case 1:
+            Navigator.of(context).pushNamed(screenNames[index]);
+            break;
+          case 2:
+            Navigator.of(context).pushNamed(screenNames[index]);
+            break;
+          case 3:
+            Navigator.of(context).pushNamed(screenNames[index]);
+            break;
+          case 4:
+            Navigator.of(context).pushNamed(screenNames[index]);
+            break;
+          default:
+            break;
+        }
+      },
+      type: BottomNavigationBarType.fixed,
+      backgroundColor: CustomColors.lightYellow,
+      unselectedLabelStyle: const TextStyle(color: CustomColors.darkGreen),
+      unselectedItemColor: CustomColors.darkGreen.withAlpha(200),
+      selectedLabelStyle: const TextStyle(
+          color: CustomColors.darkGreen, fontWeight: FontWeight.bold),
+      selectedItemColor: CustomColors.darkGreen,
+      items: items,
+    );
+  }
+}
