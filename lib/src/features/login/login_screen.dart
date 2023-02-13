323fdf82daeb9b0111b1b9c8e88b02526ec65e93
lutzzdias <thiagolutzdias@hotmail.com>
2023-02-13 14:46:26 -0400

    Improve base style but still not ready

--- lib/src/features/login/login_screen.dart
+++ lib/src/features/login/login_screen.dart
@@ -1,3 +1,4 @@
+import 'package:eldorado/src/constants/app_sizes.dart';
 import 'package:eldorado/src/features/login/login_base.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
@@ -11,49 +12,73 @@ class LoginScreen extends StatelessWidget {
       body: Stack(
         children: [
           const LoginBase(),
-          Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: [
-              SvgPicture.asset(
-                'assets/icon_and_name_white.svg',
-              ),
-              TextField(
-                decoration: InputDecoration(
-                  hintText: '230.083.390-50',
-                  hintStyle: TextStyle(color: Colors.white.withAlpha(100)),
-                  border: OutlineInputBorder(
-                    borderSide: const BorderSide(
-                      color: Colors.white,
-                      width: 3,
-                      style: BorderStyle.solid,
-                    ),
-                    borderRadius: BorderRadius.circular(10),
-                  ),
-                  filled: true,
-                  fillColor: Colors.white.withAlpha(50),
-                  prefixIcon: const Icon(Icons.person),
-                  prefixIconColor: Colors.white,
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: Sizes.p20),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                SvgPicture.asset(
+                  'assets/icon_and_name_white.svg',
                 ),
-              ),
-              const TextField(),
-              ElevatedButton(
-                onPressed: () {},
-                child: const Text('Entrar'),
-              ),
-              Row(
-                children: [
-                  TextButton(
-                    onPressed: () {},
-                    child: const Text('Esqueceu sua senha?'),
+                TextFormField(
+                  textAlign: TextAlign.center,
+                  decoration: InputDecoration(
+                    hintText: '230.083.390-50',
+                    hintStyle: TextStyle(color: Colors.white.withAlpha(100)),
+                    border: OutlineInputBorder(
+                      borderSide: const BorderSide(
+                        color: Colors.white,
+                      ),
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    filled: true,
+                    fillColor: Colors.white.withAlpha(50),
+                    prefixIcon: const Icon(Icons.person),
+                    prefixIconColor: Colors.white,
                   ),
-                  TextButton(
-                    onPressed: () {},
-                    child: const Text('Não possui acesso?'),
+                ),
+                TextFormField(
+                  textAlign: TextAlign.center,
+                  decoration: InputDecoration(
+                    hintText: '230.083.390-50',
+                    hintStyle: TextStyle(color: Colors.white.withAlpha(100)),
+                    border: OutlineInputBorder(
+                      borderSide: const BorderSide(
+                        color: Colors.white,
+                      ),
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    filled: true,
+                    fillColor: Colors.white.withAlpha(50),
+                    prefixIcon: const Icon(Icons.person),
+                    prefixIconColor: Colors.white,
                   ),
-                ],
-              )
-            ],
+                ),
+                ElevatedButton(
+                  onPressed: () {},
+                  style: ElevatedButton.styleFrom(
+                      foregroundColor: Colors.black,
+                      backgroundColor: Colors.white,
+                      textStyle: const TextStyle(fontWeight: FontWeight.w400),
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10))),
+                  child: const Text('Entrar'),
+                ),
+                Row(
+                  children: [
+                    TextButton(
+                      onPressed: () {},
+                      child: const Text('Esqueceu sua senha?'),
+                    ),
+                    TextButton(
+                      onPressed: () {},
+                      child: const Text('Não possui acesso?'),
+                    ),
+                  ],
+                )
+              ],
+            ),
           ),
         ],
       ),
