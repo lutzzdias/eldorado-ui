13d1a7ef234a97901c05cfeb607adadba02c438b
lutzzdias <thiagolutzdias@hotmail.com>
2023-02-13 14:33:49 -0400

    Create base for loginScreen

--- lib/src/features/login/login_screen.dart
+++ lib/src/features/login/login_screen.dart
@@ -1,5 +1,6 @@
 import 'package:eldorado/src/features/login/login_base.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class LoginScreen extends StatelessWidget {
   const LoginScreen({super.key});
@@ -7,7 +8,55 @@ class LoginScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: LoginBase(),
+      body: Stack(
+        children: [
+          const LoginBase(),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              SvgPicture.asset(
+                'assets/icon_and_name_white.svg',
+              ),
+              TextField(
+                decoration: InputDecoration(
+                  hintText: '230.083.390-50',
+                  hintStyle: TextStyle(color: Colors.white.withAlpha(100)),
+                  border: OutlineInputBorder(
+                    borderSide: const BorderSide(
+                      color: Colors.white,
+                      width: 3,
+                      style: BorderStyle.solid,
+                    ),
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  filled: true,
+                  fillColor: Colors.white.withAlpha(50),
+                  prefixIcon: const Icon(Icons.person),
+                  prefixIconColor: Colors.white,
+                ),
+              ),
+              const TextField(),
+              ElevatedButton(
+                onPressed: () {},
+                child: const Text('Entrar'),
+              ),
+              Row(
+                children: [
+                  TextButton(
+                    onPressed: () {},
+                    child: const Text('Esqueceu sua senha?'),
+                  ),
+                  TextButton(
+                    onPressed: () {},
+                    child: const Text('Não possui acesso?'),
+                  ),
+                ],
+              )
+            ],
+          ),
+        ],
+      ),
     );
   }
 }
