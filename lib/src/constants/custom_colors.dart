ece386ae58ee5f78227a521b0da65a7469486704
lutzzdias <thiagolutzdias@hotmail.com>
2023-02-14 11:11:47 -0400

    Add grey

--- lib/src/constants/custom_colors.dart
+++ lib/src/constants/custom_colors.dart
@@ -4,4 +4,5 @@ class CustomColors {
   static const darkGreen = Color(0xFF10331c);
   static const yellow = Color(0xFFfae84e);
   static const lightYellow = Color(0xFFe4e9b7);
+  static const gray = Color(0xFFd9d9d9);
 }
