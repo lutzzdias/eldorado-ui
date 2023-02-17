ae400b6720f90830fa6dbf16a2d02757ae917b41
lutzzdias <thiagolutzdias@hotmail.com>
2023-02-17 09:17:56 -0400

    Fix alignment bug

--- lib/src/features/observation/presentation/list_observations/widgets/observation_item_body.dart
+++ lib/src/features/observation/presentation/list_observations/widgets/observation_item_body.dart
@@ -13,6 +13,7 @@ class ObservationItemBody extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
       children: [
         const Text(
           'Observação:',
